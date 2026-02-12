54b0b14cc44410502381a8114f15a1f85f23fd1d
Saira Aguiar <142703012+sairaaguiar@users.noreply.github.com>
2026-02-12 23:11:42 +0000

    Fazendo deploy no EAS

--- expo-01/app/index.tsx
+++ expo-01/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Olá, turma</Text>
     </View>
   );
 }
